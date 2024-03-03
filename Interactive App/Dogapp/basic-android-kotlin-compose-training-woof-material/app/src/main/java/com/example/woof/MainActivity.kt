a9d5d6cd4c8f0bbc583b065eda62d40e2a4300e3
Sarvesh Khamkar <93531912+SaruDon@users.noreply.github.com>
2024-03-03 20:06:15 +0530

    wolf app

--- Interactive App/Dogapp/basic-android-kotlin-compose-training-woof-material/app/src/main/java/com/example/woof/MainActivity.kt
+++ Interactive App/Dogapp/basic-android-kotlin-compose-training-woof-material/app/src/main/java/com/example/woof/MainActivity.kt
@@ -16,29 +16,42 @@
 
 package com.example.woof
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -47,9 +60,11 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.woof.data.Dog
 import com.example.woof.data.dogs
 import com.example.woof.ui.theme.WoofTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,6 +85,8 @@ class MainActivity : ComponentActivity() {
 /**
  * Composable that displays an app bar and a list of dogs.
  */
+
+
 @Composable
 fun WoofApp() {
     Scaffold(
@@ -88,21 +105,92 @@ fun WoofApp() {
     }
 }
 
+/**
+ * Composable that displays a list item containing a dog icon and their information.
+ *
+ * @param dog contains the data that populates the list item
+ * @param modifier modifiers to set to this composable
+ */
+
+
 @Composable
 fun DogItem(
     dog: Dog,
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ) {
-    Card(modifier = modifier.padding(dimensionResource(id = R.dimen.padding_small))) {
-        Row(
-            modifier = Modifier.fillMaxWidth()
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
         ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_small))
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(Modifier.weight(1f))
+                DogItemButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded },
+                )
+            }
+            if (expanded) {
+                DogHobby(
+                    dog.hobbies, modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_medium),
+                        end = dimensionResource(R.dimen.padding_medium)
+                    )
+                )
+            }
         }
     }
 }
 
+/**
+ * Composable that displays a Top Bar with an icon and text.
+ *
+ * @param modifier modifiers to set to this composable
+ */
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_woof_logo),
+
+                    // Content Description is not needed here - image is decorative, and setting a
+                    // null content description allows accessibility services to skip this element
+                    // during navigation.
+
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
 
 /**
  * Composable that displays a photo of a dog.
@@ -130,6 +218,26 @@ fun DogIcon(
     )
 }
 
+@Composable
+fun DogHobby(
+    @StringRes dogHobby:Int,
+    modifier:Modifier = Modifier
+){
+    Column(
+        modifier = Modifier.padding(15.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.labelSmall,
+
+        )
+        Text(
+            text = stringResource(dogHobby),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
 /**
  * Composable that displays a dog's name and age.
  *
@@ -137,6 +245,29 @@ fun DogIcon(
  * @param dogAge is the Int that represents the dog's age
  * @param modifier modifiers to set to this composable
  */
+
+@Composable
+private fun DogItemButton(
+    expanded:Boolean,
+    onClick:()->Unit,
+    modifier:Modifier = Modifier
+){
+    IconButton(
+        onClick = onClick,
+        modifier= Modifier
+    ) {
+        Icon(
+            imageVector = if (expanded){
+                Icons.Filled.ExpandLess
+            } else{
+                Icons.Filled.ExpandMore
+            },
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
 @Composable
 fun DogInformation(
     @StringRes dogName: Int,
@@ -156,38 +287,24 @@ fun DogInformation(
     }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun WoofTopAppBar(modifier: Modifier =Modifier){
-    CenterAlignedTopAppBar(
-        title = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image( 
-                    modifier = Modifier
-                        .size(dimensionResource(id = R.dimen.image_size))
-                        .padding(
-                            dimensionResource(id = R.dimen.padding_small)
-                        ),
-                    painter = painterResource(id = R.drawable.ic_woof_logo) , 
-                    contentDescription = null
-                )
-                Text(text = stringResource(id = R.string.app_name),
-                    style = MaterialTheme.typography.displayLarge)
-            }
-                },
-        modifier = Modifier
-        )
-}
 /**
  * Composable that displays what the UI of the app looks like in light theme in the design tab.
  */
 @Preview
 @Composable
 fun WoofPreview() {
-    WoofTheme(darkTheme = false) {
+    WoofTheme(darkTheme = isSystemInDarkTheme()) {
+        WoofApp()
+    }
+}
+
+/**
+ * Composable that displays what the UI of the app looks like in dark theme in the design tab.
+ */
+@Preview
+@Composable
+fun WoofDarkThemePreview() {
+    WoofTheme(darkTheme = true) {
         WoofApp()
     }
 }
--- Interactive App/Dogapp/basic-android-kotlin-compose-training-woof-material/app/src/main/java/com/example/woof/ui/theme/Type.kt
+++ Interactive App/Dogapp/basic-android-kotlin-compose-training-woof-material/app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -20,21 +20,18 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.woof.R
-
-
 
 val AbrilFatface = FontFamily(
     Font(R.font.abril_fatface_regular)
 )
 
-val Montserral =FontFamily(
-    Font(R.font.montserrat_bold, FontWeight.Bold),
-    Font(R.font.abril_fatface_regular)
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
 )
-// Set of Material typography styles to start with
+
 val Typography = Typography(
     displayLarge = TextStyle(
         fontFamily = AbrilFatface,
@@ -42,19 +39,18 @@ val Typography = Typography(
         fontSize = 36.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = Montserral,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = Montserral,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = Montserral,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
-    ),
-
+    )
 )
